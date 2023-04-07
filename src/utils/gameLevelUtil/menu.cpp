cb2727fab96a63cb88ce69f6df2844e09cdfa719
Kevin V <517038@student.saxion.nl>
2023-04-07 20:22:55 +0200

    add custom arrows and add extra tekst

--- src/utils/gameLevelUtil/menu.cpp
+++ src/utils/gameLevelUtil/menu.cpp
@@ -2,25 +2,43 @@
 #include <global.h>
 #include "./utils/buttons/buttons.h"
 
-void Menu::setup() {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("> easy");
-    lcd.setCursor(2, 1);
-    lcd.print("hard");
-
-    gameDifficulty = 0;
+byte arrow[] = {
+  B00000,
+  B00000,
+  B11000,
+  B11110,
+  B11110,
+  B11000,
+  B00000,
+  B00000
 };
-
 
 void setMode(u_int8_t mode) {
     gameDifficulty = mode;
 
     lcd.setCursor(0, 0);
-    lcd.write(mode==0? '>': ' ');
+    lcd.write(mode==0? 0: ' ');
     lcd.setCursor(0, 1);
-    lcd.write(mode==1? '>': ' ');
+    lcd.write(mode==1? 0: ' ');
 }
+
+void Menu::setup() {
+
+    lcd.createChar(0, arrow);
+
+    lcd.clear();
+    lcd.setCursor(1, 0);
+    lcd.print("easy");
+    lcd.setCursor(1, 1);
+    lcd.print("hard");
+
+    lcd.setCursor(6, 0);
+    lcd.print("|Press '>'");
+    lcd.setCursor(6, 1);
+    lcd.print("|to start");
+
+    setMode(0);
+};
 
 
 void Menu::loop() {
